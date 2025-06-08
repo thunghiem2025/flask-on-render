cd7901cbf45cb04a9c1aca1106c6374faa647461
thunghiem2025 <thunghiem2025.ai@gmail.com>
2025-06-08 10:47:02 +0700

    Cập nhật nội dung app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask on Render!"
+    return "Xin chào, đây là ứng dụng đầu tiên tôi viết Flask on Render!"
 
 if __name__ == '__main__':
     app.run()
